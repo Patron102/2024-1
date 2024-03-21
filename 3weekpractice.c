315656aadf6dd0257e95b37c2cbdb1b468514087
빠뜨론 <129668361+Patron102@users.noreply.github.com>
2024-03-21 11:35:05 +0900

    Add files via upload

---
+++ 3weekpractice.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	// 1번 예제
+	printf("1번 예제\n****\n*  *\n****\n\n");
+
+	//2번 예제
+	printf("2번 예제\n\t12\n48\n\t32\n\n");
+
+	//3번 예제
+	printf("3번 예제\n%d, %f\n\n", 14, 3.14);
+
+	//4번 예제
+	int age = 10;
+	printf("4번 예제\n%d\n\n", age + 3);
+
+	//5번 예제
+	double num3 = 7 / 3;
+	printf("5번 예제\n나눗셈 : %f\n\n", num3);
+
+	//6번 예제
+	float num1;
+	num1 = sqrt(7);
+	printf("6번 예제\n루트 %d = %f\n\n", 7, num1);
+
+	return 0;
+}
