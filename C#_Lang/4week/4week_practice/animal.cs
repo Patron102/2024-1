543da4e403027faa06ddeec02f1093857517b3a2
빠뜨론 <129668361+Patron102@users.noreply.github.com>
2024-03-28 10:34:45 +0900

    d

---
+++ C#_Lang/4week/4week_practice/animal.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animal
+{
+    public string name;
+    public string sound;
+
+    public void PlaySound()
+    {
+        Debug.Log($"{name} : {sound}");
+    }
+
+}
