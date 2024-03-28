7074b844c072efa90a5dc0c39c8909274a526d43
빠뜨론 <129668361+Patron102@users.noreply.github.com>
2024-03-28 10:39:36 +0900

    d

---
+++ C#_Lang/4week/while문 연습.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class while문연습 : MonoBehaviour
+{
+    void Start()
+    {
+        int i = 0;
+
+        while (i < 10)
+        {
+            Debug.Log($"{i+1}번째 루프입니다.");
+        }
+    }
+}
