74bab23d9974d23bf17141383c3b964542a2ee93
빠뜨론 <129668361+Patron102@users.noreply.github.com>
2024-03-28 10:40:11 +0900

    d

--- C#_Lang/4week/Ins100.cs
+++ C#_Lang/4week/Ins100.cs
@@ -7,7 +7,6 @@ public class Ins100 : MonoBehaviour
     public GameObject prefab;
     void Start()
     {
-        
         for (int i = 0; i < 101; i++) 
         {
             prefab.GetComponent<Transform>().localScale = new Vector3(Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10));
