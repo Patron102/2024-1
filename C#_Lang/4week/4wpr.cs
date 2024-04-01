bfa95046ce8e9aee2379d774f9ef1cf44bffb218
빠뜨론 <129668361+Patron102@users.noreply.github.com>
2024-04-02 00:01:58 +0900

    d

---
+++ C#_Lang/4week/4wpr.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ins100 : MonoBehaviour
+{
+    public GameObject prefab;
+    void Start()
+    {
+        
+        for (int i = 0; i < 101; i++) 
+        {
+            prefab.GetComponent<Transform>().localScale = new Vector3(Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10));
+            Instantiate(prefab);
+        }
+    }
+}
